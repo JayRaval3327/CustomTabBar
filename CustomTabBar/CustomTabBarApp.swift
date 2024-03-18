25d60ec5a4e283af6aafba43ba8160026eed9f48
Jay <jraval93@gmail.com>
2024-03-18 10:59:20 -0400

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Jay Raval on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
